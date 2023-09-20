0db0a8b0d0f12d93933af98039e21f3775761956
jasmin <jasminchakma895@gmail.com>
2023-09-20 20:14:44 +0600

    about comming soon

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 
 const About = () => {
     return (
-        <div className="w-[80%] mx-auto h-screen">
+        <div className="w-[80%] mx-auto h-screen flex justify-center items-center">
             <p className="text-2xl font-semibold text-purple-300 text-center uppercase">About page comming soon....</p>
         </div>
     );
